f3f02bae2d89c829ef0f79087fd239cdc88eef07
Syuzanna Matevosyan <74954267+symatevo@users.noreply.github.com>
2022-11-30 19:23:40 +0400

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include "vector.hpp"
+
+int main() {
+    ft::vector<int> a;
+    ft::vector<int> b(3);
+    ft::vector<std::string> vector_str;
+	ft::vector<int> vector_int;
+}
---
+++ vector.hpp
@@ -0,0 +1,40 @@
+#ifndef VECTOR_HPP
+#define VECTOR_HPP
+#include <memory>
+#include <iostream>
+
+namespace ft {
+	template <typename T, typename Alloc = std::allocator<T> > class vector {
+		public:
+			typedef T	value_type;
+			typedef Alloc allocator_type;
+			typedef typename allocator_type::reference reference;
+			typedef typename allocator_type::const_reference cons_reference;
+			typedef typename allocator_type::pointer pointer;
+			typedef typename allocator_type::const_pointer const_pointer;
+			//typedef reverse_iterator<iterator> reverse_iterator;
+			//typedef reverse_iterator<con0st_iterator> const_reverse_iterator;
+			//typedef iterator_traits<iterator>::difference_type difference_type;
+			typedef size_t size_type;
+		private:
+			T* Arr;
+			size_t Capacity;
+			size_t CurrentSize;
+			Alloc myalloc;
+			value_type myval;
+			//allocator_type alloc;
+		public:
+			//ft::vector<int, std::__1::allocator<int> >::vector(unsigned long, int const&, std::__1::allocator<int> const&);
+			//vector();
+			explicit vector(const allocator_type& alloc = allocator_type());
+			explicit vector(size_type n, const value_type& val = value_type(), const allocator_type& alloc = allocator_type());
+			// template< class InputIt >
+			// vector (InputIt first, InputIt last, const allocator_type& alloc = allocator_type());
+			vector( const vector& other );
+			~vector();
+			vector &operator= (const vector &other);
+	} ;
+}
+
+# include "vector.tpp"
+#endif
